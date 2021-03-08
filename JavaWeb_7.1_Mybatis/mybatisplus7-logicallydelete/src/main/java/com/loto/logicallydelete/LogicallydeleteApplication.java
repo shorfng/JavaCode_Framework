c6dad92f52c8c3ec4a2ab9438f84252748fc44c9
蓝田_Loto <shorfng@126.com>
2021-03-08 09:41:48 +0800

    新增：MybatisPlus 逻辑删除

---
+++ JavaWeb_7.1_Mybatis/mybatisplus7-logicallydelete/src/main/java/com/loto/logicallydelete/LogicallydeleteApplication.java
@@ -0,0 +1,13 @@
+package com.loto.logicallydelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicallydeleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicallydeleteApplication.class, args);
+	}
+
+}
---
+++ JavaWeb_7.1_Mybatis/mybatisplus7-logicallydelete/src/test/java/com/loto/logicallydelete/LogicallydeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loto.logicallydelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicallydeleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
