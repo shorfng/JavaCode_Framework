d84871cf0f28fa954cce4285ef8388ef194df06d
蓝田_Loto <shorfng@126.com>
2021-04-26 14:09:48 +0800

    修改:去掉无关内容

--- JavaWeb_7.1_Mybatis/mybatis4-quickStarter-annotation/src/main/java/com/loto/pojo/User.java
+++ JavaWeb_7.1_Mybatis/mybatis4-quickStarter-annotation/src/main/java/com/loto/pojo/User.java
@@ -1,10 +1,6 @@
 package com.loto.pojo;
 
 import lombok.Data;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +14,8 @@ import java.util.List;
 
 @Data
 public class User implements Serializable {
-	@Id //对应的是注解id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //设置主键的生成策略
 	private Integer id;
-
 	private String username;
-
 	private String password;
 
 	//表示用户关联的订单
--- JavaWeb_7.1_Mybatis/mybatis5-cache/src/main/java/com/loto/mybatis/pojo/User.java
+++ JavaWeb_7.1_Mybatis/mybatis5-cache/src/main/java/com/loto/mybatis/pojo/User.java
@@ -1,10 +1,6 @@
 package com.loto.mybatis.pojo;
 
 import lombok.Data;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.io.Serializable;
 
 /**
@@ -17,11 +13,7 @@ import java.io.Serializable;
 @Data
 // 因为⼆级缓存数据存储介质多种多样，不⼀定只存在内存中，有可能存在硬盘中，如果要取这个缓存，就需要反序列化
 public class User implements Serializable {
-	@Id //对应的是注解id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //设置主键的生成策略
 	private Integer id;
-
 	private String username;
-
 	private String password;
 }
