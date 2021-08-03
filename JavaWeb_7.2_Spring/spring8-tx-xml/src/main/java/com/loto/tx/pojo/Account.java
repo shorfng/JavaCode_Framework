ed59ba0c34c5e73c5ca4210df274e6a93b0a8eef
蓝田_Loto <shorfng@126.com>
2021-08-04 00:25:25 +0800

    新增：Spring-tx，声明式事务控制，纯xml使用方式

---
+++ JavaWeb_7.2_Spring/spring8-tx-xml/src/main/java/com/loto/tx/pojo/Account.java
@@ -0,0 +1,10 @@
+package com.loto.tx.pojo;
+
+import lombok.Data;
+
+@Data
+public class Account {
+    private String cardNo;
+    private String name;
+    private int money;
+}
---
+++ JavaWeb_7.2_Spring/spring8-tx-xml/src/main/java/com/loto/tx/pojo/Result.java
@@ -0,0 +1,9 @@
+package com.loto.tx.pojo;
+
+import lombok.Data;
+
+@Data
+public class Result {
+    private String status;
+    private String message;
+}
