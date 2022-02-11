b68c7a55a240502d17d0622dbee426b1b630f8c4
蓝田_Loto <shorfng@126.com>
2022-02-11 21:09:10 +0800

    更改项目名称为 Java_9.4_Spring5

---
+++ Java_9.4_Spring5/DB/03-CreatData.sql
@@ -0,0 +1,3 @@
+# 插入 - account 转账表
+INSERT INTO `Java_9.4_Spring5`.account (name, money, cardNo) VALUES ('李大雷', 1000, '6029621011000');
+INSERT INTO `Java_9.4_Spring5`.account (name, money, cardNo) VALUES ('韩梅梅', 1000, '6029621011001');
