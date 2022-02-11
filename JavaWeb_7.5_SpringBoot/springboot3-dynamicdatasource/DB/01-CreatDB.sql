a24e7e2702bc77839ab5c8cc28c0f3f0e6a83320
蓝田_Loto <shorfng@126.com>
2022-02-11 21:09:49 +0800

    更改项目名称为 Java_9.7_SpringBoot

--- JavaWeb_7.5_SpringBoot/springboot3-dynamicdatasource/DB/01-CreatDB.sql
+++
@@ -1,6 +0,0 @@
-# 创建数据库
-CREATE database if NOT EXISTS `JavaWeb_7.5_SpringBoot-master` default character set utf8mb4 collate utf8mb4_unicode_ci;
-CREATE database if NOT EXISTS `JavaWeb_7.5_SpringBoot-slave` default character set utf8mb4 collate utf8mb4_unicode_ci;
-
-# 选择数据库
-use `JavaWeb_7.5_SpringBoot-master`;
